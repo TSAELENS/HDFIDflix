994a1fda7e55084ea00d04b12d59904c47668efb
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2026-08-01 18:05:04 +0200

    feat: Ajout des résultats pas catégorie

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -18,18 +18,48 @@ final class SearchController extends AbstractController
     ): Response {
         $form = $this->createForm(GlobalSearchType::class);
 
-        $query = $request->query->get('query');
-        $results = [];
+        $query = $request->query->getString('query');
+        $selectedType = $request->query->getString('type', 'movie');
 
-        if ($query) {
+        $groupedResults = [
+            'movie' => [],
+            'tv' => [],
+            'person' => [],
+            'other' => [],
+        ];
+
+        if ($query !== '') {
             $response = $tmdbService->search($query);
             $results = $response['results'] ?? [];
+
+            foreach ($results as $result) {
+                $mediaType = $result['media_type'] ?? 'other';
+
+                if (isset($groupedResults[$mediaType])) {
+                    $groupedResults[$mediaType][] = $result;
+                } else {
+                    $groupedResults['other'][] = $result;
+                }
+            }
+            $resultCounts = [
+                'movie' => count($groupedResults['movie']),
+                'tv' => count($groupedResults['tv']),
+                'person' => count($groupedResults['person']),
+                'other' => count($groupedResults['other']),
+            ];
+        }
+
+        if (!array_key_exists($selectedType, $groupedResults)) {
+            $selectedType = 'movie';
         }
 
         return $this->render('search/index.html.twig', [
             'form' => $form,
             'query' => $query,
-            'results' => $results,
+            'selectedType' => $selectedType,
+            'groupedResults' => $groupedResults,
+            'resultCounts' => $resultCounts,
+            'results' => $groupedResults[$selectedType],
         ]);
     }
 }
